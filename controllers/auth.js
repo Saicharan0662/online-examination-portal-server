c809cdfc75f42d369c7b5193622629a2863fcc4d
sai charan <saicharan0662@gmail.com>
2022-09-06 21:21:17 +0530

    examiner model and controllers modification

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const Student = require('../models/Student');
+const Examiner = require('../models/Examiner');
 const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
 const { UnauthenticatedError, BadRequestError } = require('../errors')
@@ -6,20 +7,33 @@ const { UnauthenticatedError, BadRequestError } = require('../errors')
 const register = async (req, res) => {
     const { name, email, password, userType } = req.body;
     const token = jwt.sign({ name, email, password }, process.env.JWT_SECRET, { expiresIn: '20m' })
-    const user = await Student.create({ name, email, password, userType });
+    let user;
+    if (userType === 'student') {
+        user = await Student.create({ name, email, password, userType });
+    } else {
+        user = await Examiner.create({ name, email, password, userType });
+    }
+
     user.sendVerificationEmail(token)
     res.status(StatusCodes.ACCEPTED).json({ user: { name: user.name, email: user.email }, msg: 'Please confirm your email' })
 }
 
 const activate = async (req, res) => {
-    const { clientToken } = req.body;
+    const { clientToken, userType } = req.body;
     let user;
     try {
         let payload = jwt.verify(clientToken, process.env.JWT_SECRET);
-        user = await Student.findOneAndUpdate({ email: payload.email }, { isActivated: true }, {
-            new: true,
-            runValidators: true
-        });
+        if (userType === 'student') {
+            user = await Student.findOneAndUpdate({ email: payload.email }, { isActivated: true }, {
+                new: true,
+                runValidators: true
+            });
+        } else {
+            user = await Examiner.findOneAndUpdate({ email: payload.email }, { isActivated: true }, {
+                new: true,
+                runValidators: true
+            });
+        }
     } catch (error) {
         throw new UnauthenticatedError("Invalid Token");
     }
---
+++ models/Examiner.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
+
+const ExaminerSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        trim: true,
+        maxLength: [20, 'Name cannot be more than 20 characters'],
+        minlength: [3, 'Name cannot be less than 3 characters'],
+    },
+    email: {
+        type: String,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: [6, 'Password cannot be less than 6 characters'],
+    },
+    isActivated: {
+        type: Boolean,
+        default: false
+    },
+    userType: {
+        type: String,
+        enum: ['student', 'examiner'],
+        default: 'examiner',
+    },
+    examsCreated: {
+        type: Array,
+        default: []
+    },
+    regStudents: {
+        type: Array,
+        default: []
+    }
+})
+
+ExaminerSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+ExaminerSchema.methods.matchPasswords = async function (clientPassword) {
+    return await bcrypt.compare(clientPassword, this.password);
+}
+
+ExaminerSchema.methods.sendVerificationEmail = async function (token) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure: true,
+        auth: {
+            user: process.env.SERVER_EMAIL,
+            pass: process.env.SERVER_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.SERVER_EMAIL,
+        to: this.email,
+        subject: 'Welcome, Please Verify Your Email',
+        html: `https://localhost:3000/activate/${token}`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.error(error);
+        } else {
+            // console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+module.exports = mongoose.model("Examiner", ExaminerSchema);
