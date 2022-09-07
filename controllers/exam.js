9d91123dc561bf6a69e1a5fd45f72c263152659f
sai charan <saicharan0662@gmail.com>
2022-09-07 16:50:45 +0530

    updating examCreated field

--- controllers/exam.js
+++ controllers/exam.js
@@ -1,4 +1,5 @@
 const Exam = require('../models/Exam');
+const Examiner = require('../models/Examiner');
 const { UnauthenticatedError, BadRequestError } = require('../errors')
 const { StatusCodes } = require('http-status-codes');
 
@@ -9,6 +10,9 @@ const createExam = async (req, res) => {
         throw new BadRequestError("Please provide the required fields");
 
     const exam = await Exam.create({ name, description, duration, topics, questions, createdBy: req.user.userID });
+    const examiner = await Examiner.findByIdAndUpdate({ _id: req.user.userID },
+        { $push: { examsCreated: exam._id } },
+        { new: true, runValidators: true });
 
     res.status(StatusCodes.CREATED).json({ exam, msg: "success" })
 }
