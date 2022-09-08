c7ea2c34c9f946302789c71b8e7e3960960ec1d7
sai charan <saicharan0662@gmail.com>
2022-09-08 23:06:15 +0530

    added cros middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 const connectDB = require('./db/connect');
@@ -9,6 +10,7 @@ const examRoute = require('./routes/exam');
 const authMiddleware = require('./middleware/authentication');
 const ErrorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/exam', authMiddleware, examRoute);
