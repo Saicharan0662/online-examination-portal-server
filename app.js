cb0a7097319689557de25dd4a947f003250d7b72
sai charan <saicharan0662@gmail.com>
2023-03-28 21:53:13 +0530

    removed socket code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const socketIO = require('socket.io');
 const http = require('http')
 const cors = require('cors')
 
@@ -22,28 +21,11 @@ app.use('/api/v1/exam', authMiddleware, examRoute);
 app.use('/api/v1/result', authMiddleware, resultRoute);
 app.use(ErrorHandlerMiddleware)
 
-// sockets
-server.listen(5001, () => console.log('listening on port 5001'))
-const io = socketIO(server, {
-    cors: {
-        origin: '*',
-    }
-});
-io.on('connection', (socket) => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
-        socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId);
-        });
-    });
-});
-
 // server start
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(process.env.PORT, () => {
+        server.listen(process.env.PORT, () => {
             console.log(`Server started on port ${process.env.PORT}`);
         })
     } catch (error) {
